0f2f56f4c17daf31b08d6ae87e374b277ae9f418
Datta <dattakadiyala@gmail.com>
2016-11-21 02:09:27 -0600

    Initial Commit

--- Speak Number/ViewController.swift
+++ Speak Number/ViewController.swift
@@ -1,17 +1,34 @@
 //
 //  ViewController.swift
-//  Speak Number
+//  Speak Token Number
 //
-//  Created by Datta on 11/20/16.
+//  Created by Datta on 11/19/16.
 //  Copyright © 2016 dvapps. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
+    @IBOutlet weak var tokenNumberText: UITextField!
+    var synthesizer = AVSpeechSynthesizer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            print("AVAudioSession Category Playback OK")
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+                print("AVAudioSession is Active")
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +37,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func speakButton(_ sender: Any) {
+        let text = tokenNumberText.text
+        let utterance = AVSpeechUtterance(string: text!)
+        utterance.voice = AVSpeechSynthesisVoice(language : "en-GB")
+        synthesizer.speak(utterance)
+        
+        
+    }
+    @IBAction func afterNumber(_ sender: Any) {
+        if var tempText: Int = Int(tokenNumberText.text!){
+            tempText += 1
+            tokenNumberText.text = String(tempText)
+        }
+        
+    }
+    
+    
+    @IBAction func beforeNumber(_ sender: Any) {
+        if var tempText: Int = Int(tokenNumberText.text!){
+            tempText -= 1
+            tokenNumberText.text = String(tempText)
+        }
+    
+    }
+    
 
 }
-
